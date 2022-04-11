1e975c257fde31f46abb760b0b28ca385123b6ba
talkinglibrary <nashvilletalkinglibrary@gmail.com>
2022-04-11 15:47:59 -0500

    cleanup, file added.
    adding console_log HTML file for example.

--- TLNowPlaying.js
+++ TLNowPlaying.js
@@ -5,13 +5,18 @@ prints to browser console for testing
 © Ben Weddle is to blame for this code
  */
 
-fetch('https://npl.streamguys1.com/status.xsl') //http is much faster than https
-.then(response => response.text())
-.then(function(html) {
-    var dom = new DOMParser();
-    var parse = dom.parseFromString(html, "text/html"); 
-    var index = 21; //index of 'current song'
-    var result = parse.querySelectorAll('td')[index].textContent; //parse page for tag at index position and return textcontext field
-    
-    console.log(result) //for testing
-})
+function nowplaying() {
+
+    fetch('http://npl.streamguys1.com/status.xsl') //http is much faster than https
+    .then(response => response.text())
+    .then(function(html) {
+        var dom = new DOMParser();
+        var parse = dom.parseFromString(html, "text/html"); 
+        var index = 21; //index of 'current song'
+        var result = parse.querySelectorAll('td')[index].textContent; //parse page for tag at index position and return textcontext field
+        
+        console.log(result)
+    })
+}
+nowplaying()
+setInterval(nowplaying, 30000) //1000 = 1 second
